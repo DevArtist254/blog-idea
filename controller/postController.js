aa2c313d03784be7573f3b21814c2e0dc9a299e3
DevArtist254 <kevinwasonga415@outlook.com>
2023-08-07 03:39:05 +0300

    CRUD

--- controller/postController.js
+++ controller/postController.js
@@ -1,20 +1,79 @@
 const Post = require('./../model/postModel');
 
-exports.getAllPost = (req, res) => {
-  res.status(200).json({
-    backendTest: 'hello world',
-  });
+exports.getAllPost = async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json({
+      length: posts.length,
+      data: posts,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 exports.createAPost = async (req, res) => {
   try {
     const post = await Post.create(req.body);
 
-    res.status(200).json({
+    return res.status(200).json({
       createPost: post,
     });
   } catch (error) {
-    res.status(404).json({
+    return res.status(404).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.getAPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        error: error.message,
+      });
+    }
+
+    return res.status(200).json({
+      data: post,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.updateAPost = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const post = await Post.findByIdAndUpdate(id, body);
+
+    return res.status(200).json({
+      data: post,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteAPost = async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+
+    return res.status(204).json({
+      message: 'Content deleted',
+    });
+  } catch (error) {
+    return res.status(404).json({
       error: error.message,
     });
   }
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const {getAllPost, createAPost} = require('./../controller/postController');
+const {
+  getAllPost,
+  createAPost,
+  getAPost,
+  updateAPost,
+  deleteAPost,
+} = require('./../controller/postController');
 
 const router = express.Router();
 
 router.get('/', getAllPost);
 router.post('/', createAPost);
+
+router.get('/:id', getAPost);
+router.patch('/:id', updateAPost);
+router.delete('/:id', deleteAPost);
 
 module.exports = router;
